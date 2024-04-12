435eceda88e43c1ddcdc43aca4d139f57ac87a28
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2024-04-12 21:49:35 +0530

    feat: zod validation for api requests

--- app/lib/util.ts
+++ app/lib/util.ts
@@ -1,3 +1,5 @@
+import { z } from "zod"
+
 export function absoluteUrl(path: string) {
     if (typeof window !== 'undefined') return path
     if (process.env.VERCEL_URL)
@@ -5,3 +7,16 @@ export function absoluteUrl(path: string) {
     return `http://localhost:${process.env.PORT ?? 3000
         }${path}`
 }
+
+export const sessionInputValidator = z.object({
+    priceId: z.string().min(1, {
+        message: 'Priceid required',
+    }),
+})
+
+
+export const stripeProcessPaymentValidator = z.object({
+    session_id: z.string().min(1, {
+        message: 'Session id required',
+    }),
+})
