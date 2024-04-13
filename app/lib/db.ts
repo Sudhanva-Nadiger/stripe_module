2f411694e18e794ba31d57466c90b0aea560aac8
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2024-04-13 11:19:41 +0530

    feat: inmemory db

--- app/lib/db.ts
+++
@@ -1,9 +0,0 @@
-type SubscriptionDetail = {
-    userId: string
-    stripeSubscriptionId: string
-    stripeCustomerId: string
-    stripePriceId: string,
-    stripeCurrentPeriodEnd: Date,
-}
-
-export const subscriptionDetails: SubscriptionDetail[] = []
